2a1e839b7999ef849e5f472eeda7d7c37f00eb8f
Manideep2363 <vannemanideep@gmail.com>
2025-12-02 15:03:11 +0530

    update-12-02-25

---
+++ lab12/row_number.py
@@ -0,0 +1,6 @@
+i=1
+while i<=100:
+    for j in range(5):
+        print(i,end=" ")
+        i+=1
+    print()
---
+++ lab26/pattern1.py
@@ -0,0 +1,6 @@
+for i in range(6):
+    for j in range(1,i+1):
+        print(j, end=' ')
+    for k in range(1):
+        print('1', end=' ')
+    print()
